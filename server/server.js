c4241966ed6673611422a939cbef36ad1aef0735
LoN <theseashoreph@gmail.com>
2025-04-01 14:58:45 +0800

    success connection on hostgator

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 import express from 'express'
+import cors from 'cors'
+import mysql from 'mysql'
+
+import 'dotenv/config'
 
 const app = express()
 const port = process.env.PORT || 8080
 
+const salt = 10
+
+const db = mysql.createConnection({
+  host: '192.185.17.41',
+  user: 'webadmin_chinabank',
+  password: 'chinaAdmin!',
+  database: 'webadmin_crud'
+})
+
 app.get('/', (req, res) => {
   res.send('server/')
+})
+
+app.get('/get/', (_req, res) => {
+  const q = 'select * from users'
+  db.query(q, (err, result) => {
+    if (err) return res.json(err)
+    return res.json(result)
+  })
+
 })
 
 app.listen(port, () => { console.log('Local Server running on PORT:', port) })
